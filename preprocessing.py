5a91671372edd9b065e745a2b4bf04e3de9eedb6
yeajoo <ziousarang@naver.com>
2025-06-08 19:04:22 +0900

    전처리코드 각주 추가

--- preprocessing.py
+++ preprocessing.py
@@ -3,13 +3,13 @@ import numpy as np
 import re
 
 #트위터피드 전처리
-file_path = "2018_metoo.csv"
+file_path = "2018_bmw.csv" #옥토퍼스로 수집한 트위터 피드 csv파일 불러오기
 
 df = pd.read_csv(file_path)
-tweet_contents = df["Tweet_Content"].dropna() #Tweet_Content 열의 내용만 추출
-tweet_contents.to_csv("2018_metoo.txt", index=False, header=True, lineterminator='\n', encoding='utf-8') #txt파일로 변환 후 저장
+tweet_contents = df["Tweet_Content"].dropna() #헤더가 Tweet_Content인 열의 내용만 추출
+tweet_contents.to_csv("2018_bmw.txt", index=False, header=True, lineterminator='\n', encoding='utf-8') #txt파일로 변환 후 저장
 
-train_data = pd.read_table("2018_metoo.txt")
+train_data = pd.read_table("2018_bmw.txt")
 print(train_data[:5])
 print(len(train_data))
 
@@ -35,7 +35,7 @@ def preprocessing(text):
 
 print(train_data["Tweet_Content"][:5])
 train_data["Tweet_Content"] = train_data["Tweet_Content"].apply(preprocessing)
-print(train_data["Tweet_Content"][:5])
+print(train_data["Tweet_Content"][:5]) #정규표현식으로 정제하여 36행과 비교
 
 train_data["Tweet_Content"].replace('', np.nan, inplace=True)
 print(train_data.isnull().sum()) #0
@@ -44,12 +44,12 @@ print(train_data.isnull().sum()) #0
 # train_data = train_data.dropna(how = 'any')
 # print(len(train_data)) #45행 출력값이 1 이상이라면 실행
 
-train_data.to_csv("2024_parisolympic_cleaned.txt", index=False, sep='\t', encoding='utf-8') #전처리 텍스트 txt파일로 저장
+train_data.to_csv("2018_bmw_cleaned.txt", index=False, sep='\t', encoding='utf-8') #전처리 텍스트 txt파일로 저장
 
 
-
-input_file = "2024_parisolympic_cleaned.txt"
-output_file = "2024_parisolympic_cleaned1.txt"
+#51~69행: 키워드(검색어)를 포함하지 않는 행은 제거, 필요한 경우 선택적으로 수행
+input_file = "2018_bmw_cleaned.txt"
+output_file = "2018_bmw_cleaned2.txt"
 
 with open(input_file, 'r', encoding='utf-8') as f:
     lines = f.readlines()
@@ -60,26 +60,24 @@ print(len(lines))
 header = lines[0]
 content_lines = lines[1:]
 
-# '버닝썬'이 포함된 행만 필터링
-filtered_lines = [line for line in content_lines if '올림픽' in line]
+#키워드인 'bmw'와 '리콜'이 포함된 행만 필터링
+filtered_lines = [line for line in content_lines if 'bmw' in line and '리콜' in line]
 print(len(filtered_lines))
 
 # 헤더 + 필터링된 내용 저장
 with open(output_file, 'w', encoding='utf-8') as f:
     f.writelines([header] + filtered_lines)
-#51~69행 올림픽을 포함하지 않는 행은 제거, 필요한 경우 수행
-
 
 
 
 #유튜브 댓글 전처리
-file_path = "youtube(2019,worldcup)/comments.csv"
+file_path = "2019_worldcup_comments.csv"
 
 df = pd.read_csv(file_path)
-youtube_comments = df["text"].dropna() #text 열의 내용만 추출
-youtube_comments.to_csv("youtube(2019,worldcup)/comments.txt", index=False, header=True, lineterminator='\n', encoding='utf-8') #txt파일로 변환 후 저장
+youtube_comments = df["text"].dropna() #헤더가 text인 열의 내용만 추출
+youtube_comments.to_csv("2019_worldcup_comments.txt", index=False, header=True, lineterminator='\n', encoding='utf-8') #txt파일로 변환 후 저장
 
-train_data = pd.read_table("youtube(2019,worldcup)/comments.txt")
+train_data = pd.read_table("2019_worldcup_comments.txt")
 print(train_data[:5])
 print(len(train_data))
 
@@ -93,7 +91,7 @@ print(train_data.isnull().values.any()) #False
 # train_data.loc[train_data.comment1.isnull()]
 # train_data = train_data.dropna(how = 'any')
 # print(train_data.isnull().values.any())
-# print(len(train_data)) #90행에서 출력값이 True라면 실행
+# print(len(train_data)) #88행에서 출력값이 True라면 실행
 
 def preprocessing(text):
     new_text1 = re.sub(r"\(.*?\)", "", text)
@@ -104,13 +102,13 @@ def preprocessing(text):
 
 print(train_data["text"][:5])
 train_data["text"] = train_data["text"].apply(preprocessing)
-print(train_data["text"][:5])
+print(train_data["text"][:5]) #정규표현식을 통해 정제한 후 103행과 비교
 
 train_data["text"].replace('', np.nan, inplace=True)
 print(train_data.isnull().sum()) #0
 
-print(train_data.loc[train_data.text.isnull()][:5])
-train_data = train_data.dropna(how = 'any')
-print(len(train_data)) #110행 출력값이 1 이상이라면 실행
+#print(train_data.loc[train_data.text.isnull()][:5])
+#train_data = train_data.dropna(how = 'any')
+#print(len(train_data)) #108행 출력값이 1 이상이라면 실행
 
-train_data.to_csv("youtube(2019,worldcup)/comments_cleaned.txt", index=False, sep='\t', encoding='utf-8') #전처리 텍스트 txt파일로 저장
+train_data.to_csv("2019_worldcup_comments_cleaned.txt", index=False, sep='\t', encoding='utf-8') #전처리 텍스트 txt파일로 저장
